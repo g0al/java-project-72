0a0a903c28ddbc84358a735f4445b112ad7a1fe8
g0al <goalenator@gmail.com>
2025-04-08 20:37:00 +0300

    first commit

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,182 @@
+package hexlet.code;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.sql.SQLException;
+//import java.util.List;
+import java.util.stream.Collectors;
+
+import io.javalin.rendering.template.JavalinJte;
+
+import static io.javalin.rendering.template.TemplateUtil.model;
+
+//import org.example.hexlet.controller.CarsController;
+//import org.example.hexlet.controller.PostsController;
+//import org.example.hexlet.controller.SessionsController;
+//import org.example.hexlet.controller.UsersController;
+import hexlet.code.dto.MainPage;
+//import org.example.hexlet.dto.courses.CoursesPage;
+//import org.example.hexlet.dto.courses.CoursePage;
+//import org.example.hexlet.dto.users.BuildUserPage;
+//import org.example.hexlet.dto.users.UsersPage;
+//import org.example.hexlet.model.Course;
+//import org.example.hexlet.model.User;
+//import org.example.hexlet.repository.BaseRepository;
+//import org.example.hexlet.repository.CourseRepository;
+//import org.example.hexlet.repository.UserRepository;
+//import hexlet.code.util.NamedRoutes;
+
+//import com.zaxxer.hikari.HikariConfig;
+//import com.zaxxer.hikari.HikariDataSource;
+
+import io.javalin.Javalin;
+//import io.javalin.validation.ValidationException;
+//import io.javalin.http.NotFoundResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.valueOf(port);
+    }
+
+    private static String readResourceFile(String fileName) throws IOException {
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return reader.lines().collect(Collectors.joining("\n"));
+        }
+    }
+
+    public static void main(String[] args) throws IOException, SQLException {
+        var app = getApp();
+
+        app.start(getPort());
+    }
+
+    public static Javalin getApp() throws IOException, SQLException {
+        // System.setProperty("h2.traceLevel", "TRACE_LEVEL_SYSTEM_OUT=4");
+
+        //var hikariConfig = new HikariConfig();
+        //hikariConfig.setJdbcUrl("jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+
+        //var dataSource = new HikariDataSource(hikariConfig);
+        //var sql = readResourceFile("schema.sql");
+        /*
+        log.info(sql);
+        try (var connection = dataSource.getConnection();
+             var statement = connection.createStatement()) {
+            statement.execute(sql);
+        }
+        BaseRepository.dataSource = dataSource;
+        */
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.before(ctx -> {
+            ctx.contentType("text/html; charset=utf-8");
+        });
+        /*
+        app.get("/sessions/build", SessionsController::build);
+        app.post("/sessions", SessionsController::create);
+        app.delete("/sessions", SessionsController::destroy);
+
+        app.get("/posts", PostsController::index);
+        app.get("/posts/{id}", PostsController::show);
+        app.get("/posts/build", PostsController::build);
+        app.post("/posts", PostsController::create);
+        app.get("/posts/{id}/edit", PostsController::edit);
+        app.patch("/posts/{id}", PostsController::update);
+        app.delete("/posts", PostsController::destroy);
+
+        app.get("/cars", CarsController::index);
+        app.get("/cars/build", CarsController::build);
+        app.get("/cars/{id}", CarsController::show);
+        app.post("/cars", CarsController::create);
+        */
+        app.get("/", ctx -> {
+            var visited = Boolean.valueOf(ctx.cookie("visited"));
+            var page = new MainPage(visited, ctx.sessionAttribute("currentUser"));
+            ctx.render("index.jte", model("page", page));
+            ctx.cookie("visited", String.valueOf(true));
+        });
+        /*
+        app.get(NamedRoutes.buildUserPath(), ctx -> {
+            var page = new BuildUserPage();
+            ctx.render("users/build.jte", model("page", page));
+        });
+
+        app.get("/users/{id}", UsersController::show);
+
+        app.post(NamedRoutes.usersPath(), ctx -> {
+            var name = ctx.formParam("name").trim();
+            var email = ctx.formParam("email").trim().toLowerCase();
+
+            try {
+                var passwordConfirmation = ctx.formParam("passwordConfirmation");
+                var password = ctx.formParamAsClass("password", String.class)
+                        .check(value -> value.equals(passwordConfirmation), "Passwords are not the same")
+                        .check(value -> value.length() > 6, "Password is to short")
+                        .get();
+                var user = new User(name, email, password);
+                UserRepository.save(user);
+                ctx.redirect(NamedRoutes.usersPath());
+            } catch (ValidationException e) {
+                var page = new BuildUserPage(name, email, e.getErrors());
+                ctx.render("users/build.jte", model("page", page));
+            }
+        });
+
+        app.get(NamedRoutes.usersPath(), ctx -> {
+            var users = UserRepository.getEntities();
+            var page = new UsersPage(users);
+            // Отдаем обратно url + query params
+            ctx.render("users/index.jte", model("page", page));
+        });
+
+        app.get(NamedRoutes.buildCoursePath(), ctx -> {
+            ctx.render("courses/build.jte");
+        });
+
+        app.get(NamedRoutes.coursesPath(), ctx -> {
+            var term = ctx.queryParam("term");
+            // ctx.sessionAttribute("key", "value");
+            List<Course> courses;
+            if (term != null) {
+                // Фильтруем курсы в соответствии со значением term
+                courses = CourseRepository.search(term);
+            } else {
+                courses = CourseRepository.getEntities();
+            }
+            var page = new CoursesPage(courses, term);
+            page.setFlash(ctx.consumeSessionAttribute("flash"));
+
+            ctx.render("courses/index.jte", model("page", page));
+        });
+
+        app.get(NamedRoutes.coursePath("{id}"), ctx -> {
+            var id = ctx.pathParamAsClass("id", Long.class).get();
+            var course = CourseRepository.find(id)
+                    .orElseThrow(() -> new NotFoundResponse("Entity with id = " + id + " not found"));
+            var page = new CoursePage(course);
+            ctx.render("courses/show.jte", model("page", page));
+        });
+
+        app.post(NamedRoutes.coursesPath(), ctx -> {
+            var name = ctx.formParam("name");
+            var description = ctx.formParam("description");
+
+            var course = new Course(name, description);
+            CourseRepository.save(course);
+            ctx.sessionAttribute("flash", "Course has been created!");
+            ctx.redirect(NamedRoutes.coursesPath());
+        });
+         */
+        return app;
+    }
+}
