24ce14b6171fbac22ac11e1060ca96dd891d917c
g0al <goalenator@gmail.com>
2025-06-07 14:23:38 +0300

    fix mistakes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,4 +59,8 @@ tasks.test {
 
 tasks.jacocoTestReport {
     dependsOn(tasks.test) // tests are required to run before generating the report
+    reports {
+        xml.required.set(true)
+        html.required.set(false)
+    }
 }
